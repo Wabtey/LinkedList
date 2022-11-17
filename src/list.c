420c118da8c5872ed7cb3a2790e9c99315b7e55d
Wabtey <samurai.banzai@laposte.net>
2022-11-17 12:57:30 +0100

    format

--- src/list.c
+++ src/list.c
@@ -25,10 +25,11 @@
  *   NULL in case of error, otherwise a pointer over one strusture of type list_elem_t
  */
 static list_elem_t *
-create_element (int value)
+create_element(int value)
 {
-  list_elem_t * newelt = malloc (sizeof (list_elem_t));
-  if (newelt != NULL) {
+  list_elem_t *newelt = malloc(sizeof(list_elem_t));
+  if (newelt != NULL)
+  {
     newelt->value = value;
     newelt->next = NULL;
   }
@@ -48,12 +49,17 @@ create_element (int value)
  *    0 in case of successful insertion
  *   -1 if the insertion is impossible
  */
-int
-insert_head (list_elem_t * * l, int value)
+int insert_head(list_elem_t **l, int value)
 {
-  if (l == NULL) { return -1; }
-  list_elem_t * new_elt = create_element (value);
-  if (new_elt == NULL) { return -1; }
+  if (l == NULL)
+  {
+    return -1;
+  }
+  list_elem_t *new_elt = create_element(value);
+  if (new_elt == NULL)
+  {
+    return -1;
+  }
 
   new_elt->next = *l;
   *l = new_elt;
@@ -72,38 +78,40 @@ insert_head (list_elem_t * * l, int value)
  *    0 in case of successful insertion
  *   -1 if the insertion is impossible
  */
-int
-insert_tail(list_elem_t * * l, int value) {
+int insert_tail(list_elem_t **l, int value)
+{
 
-  if(l==NULL){ return -1; } 
-  list_elem_t * head = *l;
-  list_elem_t * newTail = create_element(value);
+  if (l == NULL)
+  {
+    return -1;
+  }
+  list_elem_t *head = *l;
+  list_elem_t *newTail = create_element(value);
 
   // browse the list
-  if ( head != NULL ) {
+  if (head != NULL)
+  {
 
-    list_elem_t * current = head;
-    
+    list_elem_t *current = head;
+
     // current's linked to head
-    head -> next = current->next;
+    head->next = current->next;
 
-    while(current -> next != NULL)
+    while (current->next != NULL)
     {
-      current = current -> next;
+      current = current->next;
     }
 
-    current -> next = newTail;
-
+    current->next = newTail;
   }
-  // if the list is empty 
-  else {
+  // if the list is empty
+  else
+  {
     head = newTail;
   }
 
-  *l=head;
+  *l = head;
   return 0;
- 
-
 }
 
 /*
@@ -119,15 +127,19 @@ insert_tail(list_elem_t * * l, int value) {
  *   - a pointer to the element of the list in case of success
  *   - NULL in case of error
  */
-list_elem_t * find_element(list_elem_t * l, int index) {
+list_elem_t *find_element(list_elem_t *l, int index)
+{
 
-  list_elem_t * current = l;
+  list_elem_t *current = l;
 
-  for (int i = 0 ; i < index ; i++)
+  for (int i = 0; i < index; i++)
   {
     // si l'element est null on est deja à la fin de la liste et on a pas trouvé l'element
-    if ( current==NULL ) { return NULL; } 
-    current = current -> next;
+    if (current == NULL)
+    {
+      return NULL;
+    }
+    current = current->next;
   }
 
   return current;
@@ -145,26 +157,31 @@ list_elem_t * find_element(list_elem_t * l, int index) {
  *   - an index to the element
  *   - NULL in case of error
  */
-int
-find_index_element(list_elem_t * l, int value) {
+int find_index_element(list_elem_t *l, int value)
+{
 
-  list_elem_t * current = l;
+  list_elem_t *current = l;
 
-  if ( current==NULL ) { return -1; } 
-  
+  if (current == NULL)
+  {
+    return -1;
+  }
+
   int index = 0;
 
-  while ( current->value != value )
+  while (current->value != value)
   {
-    current = current -> next;
+    current = current->next;
     index++;
     // on a pas trouvé l'element cherché
-    if ( current==NULL ) { return -1; } 
+    if (current == NULL)
+    {
+      return -1;
+    }
   }
 
   return index;
 }
-
 
 /*
  * SYNOPSYS :
@@ -184,59 +201,124 @@ find_index_element(list_elem_t * l, int value) {
  *   - NULL in case of error
  */
 head_parent_target
-find_first_element_with_value(list_elem_t * l, int value) {
+find_first_element_with_value(list_elem_t *l, int value)
+{
 
-    list_elem_t * current = l;
-    // list_elem_t * null = NULL;
+  list_elem_t *current = l;
+  // list_elem_t * null = NULL;
 
-    if ( current==NULL ) {
-        head_parent_target res;
-        res.head = l;
-        res.parent = *(list_elem_t *) NULL;
-        res.target = *(list_elem_t *) NULL;
+  if (current == NULL)
+  {
+    head_parent_target res;
+    res.head = l;
+    res.parent = *(list_elem_t *)NULL;
+    res.target = *(list_elem_t *)NULL;
 
-        // res.parent = *null;
-        // res.target = *null;
-        return res;
-    } 
-    
-    // without && current-> next != NULL
-    while ( current->value != value )
+    // res.parent = *null;
+    // res.target = *null;
+    return res;
+  }
+
+  // without && current-> next != NULL
+  while (current->value != value)
+  {
+    list_elem_t *parent = current;
+    current = current->next;
+    if (current == NULL)
     {
-      list_elem_t * parent = current;
-      current = current -> next;
-      if ( current==NULL ) {
-          head_parent_target res;
-          res.head = l;
-          res.parent = *parent;
-          // segmentation fault obtains with remove_test.txt
-          res.target = *(list_elem_t *) NULL;
-          return res;
-      } 
-      else if ( current->value == value ) {
-          // should return the parent to be used in remove()
-          // AND return the head aswell containing the entire list
-          head_parent_target res;
-          res.head = l;
-          res.parent = *parent;
-          res.target = *current;
-          return res;
+      head_parent_target res;
+      res.head = l;
+      res.parent = *parent;
+      // segmentation fault obtains with remove_test.txt
+      res.target = *(list_elem_t *)NULL;
+      return res;
+    }
+    else if (current->value == value)
+    {
+      // should return the parent to be used in remove()
+      // AND return the head aswell containing the entire list
+      head_parent_target res;
+      res.head = l;
+      res.parent = *parent;
+      res.target = *current;
+      return res;
+    }
+  }
+
+  head_parent_target res;
+  res.head = l;
+  res.parent = *(list_elem_t *)NULL;
+  res.target = *(list_elem_t *)NULL;
+  return res;
+}
+
+/*
+ * SYNOPSYS :
+ *   int remove_element(list_elem_t * * ppl, int value)
+ * DESCRIPTION :
+ *   Removes from the list the first element with a value equal to the argument value and
+ *   frees the memory space reserved by this element.
+ *   Attention : Depending on the position, the head of the list may need to be modified
+ * PARAMETERS :
+ *   list_elem_t ** ppl : pointer to the pointer of the head of the list
+ *   int value  : value to be removed from the list
+ * RESULT :
+ *    0 in case of success
+ *   -1 in case of error
+ */
+
+int remove_element(list_elem_t **ppl, int value)
+{
+
+  list_elem_t *current = *ppl;
+
+  // liste vide
+  if (current == NULL)
+  {
+    return -1;
+  }
+
+  // le premier element doit etre supprimer
+  if (current->value == value)
+  {
+    list_elem_t *removing = current;
+    current = current->next;
+    *ppl = current;
+    free(removing);
+    return 0;
+  }
+  else
+  {
+
+    list_elem_t *before = current;
+    current = current->next;
+
+    while (current != NULL)
+    {
+      if (current->value == value)
+      {
+
+        before->next = current->next;
+        free(current);
+
+        return 0;
+      }
+      else
+      {
+        before = current;
+        current = current->next;
       }
     }
+  }
 
-    head_parent_target res;
-          res.head = l;
-          res.parent = *(list_elem_t *) NULL;
-          res.target = *(list_elem_t *) NULL;
-    return res;
+  return -1;
 }
-
 
 /*
  * SYNOPSYS :
  *   int remove_element(list_elem_t * * ppl, int value)
  * DESCRIPTION :
- *   Removes from the list the first element with a value equal to the argument value and 
+ *   Removes from the list the first element with a value equal to the argument value and
  *   frees the memory space reserved by this element.
  *   Attention : Depending on the position, the head of the list may need to be modified
  * PARAMETERS :
@@ -246,100 +328,48 @@ find_first_element_with_value(list_elem_t * l, int value) {
  *    0 in case of success
  *   -1 in case of error
  */
- 
-int
-remove_element(list_elem_t * * ppl, int value) { 
+int remove_element(list_elem_t **ppl, int value)
+{
+  head_parent_target headParentTarget = find_first_element_with_value(*ppl, value);
 
-	list_elem_t * current = *ppl;
-	
-	//liste vide 
-	if (current == NULL) { return -1; }
-	
-	//le premier element doit etre supprimer
-	if (current -> value == value){
-  		list_elem_t * removing = current;
-  		current = current -> next;
-  		*ppl = current;
-  		free(removing);
-  		return 0;	
-  	}else{
-  	
-		list_elem_t * before = current;
-		current = current->next;
+  list_elem_t *head = headParentTarget.head;
+  list_elem_t *parent = &headParentTarget.parent;
+  list_elem_t *target = &headParentTarget.target;
 
-		while(current != NULL){
-			if (current -> value == value){
-				
-				before -> next = current ->next ;
-				free(current);
-				
-				return 0;	
-			}
-			else{
-				before = current;
-				current = current->next;
-			}
-		}
-	}
-	
-	return -1;
-
-}
-
-
-/*
- * SYNOPSYS :
- *   int remove_element(list_elem_t * * ppl, int value)
- * DESCRIPTION :
- *   Removes from the list the first element with a value equal to the argument value and 
- *   frees the memory space reserved by this element.
- *   Attention : Depending on the position, the head of the list may need to be modified
- * PARAMETERS :
- *   list_elem_t ** ppl : pointer to the pointer of the head of the list
- *   int value  : value to be removed from the list
- * RESULT :
- *    0 in case of success
- *   -1 in case of error
- */
-int
-remove_element(list_elem_t * * ppl, int value) {
-     head_parent_target headParentTarget = find_first_element_with_value(*ppl, value);
-
-     list_elem_t * head = headParentTarget.head;
-     list_elem_t * parent = &headParentTarget.parent;
-     list_elem_t * target = &headParentTarget.target;
-
-     if (
-        head != NULL   &&
-        parent != NULL &&
-        target != NULL
-     ) {
-          parent->next =  target->next;
-          ppl = &head;
-          free(target);
-          return 0;
-     }
-     else { return -1; }
-    //  switch( headParentTarget ) {
-    //       case (NULL, NULL, NULL):
-    //       case (head, NULL, NULL):
-    //       case (head, parent, NULL): return -1; // break;
-    //       case (head, parent, target): 
-    //            // redundant if
-    //            if (target != NULL) {
-    //                 parent->next =  target->next;
-    //                 ppl = *head;
-    //                 free(target);
-    //            }
-    //            return 0;
-    //            // break;
-    //  }
+  if (
+      head != NULL &&
+      parent != NULL &&
+      target != NULL)
+  {
+    parent->next = target->next;
+    ppl = &head;
+    free(target);
+    return 0;
+  }
+  else
+  {
+    return -1;
+  }
+  //  switch( headParentTarget ) {
+  //       case (NULL, NULL, NULL):
+  //       case (head, NULL, NULL):
+  //       case (head, parent, NULL): return -1; // break;
+  //       case (head, parent, target):
+  //            // redundant if
+  //            if (target != NULL) {
+  //                 parent->next =  target->next;
+  //                 ppl = *head;
+  //                 free(target);
+  //            }
+  //            return 0;
+  //            // break;
+  //  }
 }
 /*
  * SYNOPSYS :
  *   int remove_element_2(list_elem_t * * ppl, int value)
  * DESCRIPTION :
- *   Removes from the list the first element with a value equal to the argument value and 
+ *   Removes from the list the first element with a value equal to the argument value and
  *   frees the memory space reserved by this element.
  *   Attention : Depending on the position, the head of the list may need to be modified
  * use an auxiliar function to find the index of theelement to remove
@@ -350,37 +380,40 @@ remove_element(list_elem_t * * ppl, int value) {
  *    0 in case of success
  *   -1 in case of error
  */
-int
-remove_element_2(list_elem_t * * ppl, int value) {
-  list_elem_t * before = *ppl;
-  if (before == NULL) { return -1; }
+int remove_element_2(list_elem_t **ppl, int value)
+{
+  list_elem_t *before = *ppl;
+  if (before == NULL)
+  {
+    return -1;
+  }
 
-  int indexremove = find_index_element(*ppl,value);
-  printf( "index : %d/n", indexremove);
-  if(indexremove==-1) {
-  	printf("l'élément n'est pas présent dans la liste");
-  	return -1;
+  int indexremove = find_index_element(*ppl, value);
+  printf("index : %d/n", indexremove);
+  if (indexremove == -1)
+  {
+    printf("l'élément n'est pas présent dans la liste");
+    return -1;
   }
-  else if (indexremove==0){
-  	list_elem_t * removing = before;
-  	before = before -> next;
-  	*ppl = before;
-  	free(removing);  	
-  	
+  else if (indexremove == 0)
+  {
+    list_elem_t *removing = before;
+    before = before->next;
+    *ppl = before;
+    free(removing);
   }
-  else{
-  	for (int i = 0; i<indexremove-1; i++){
-  		before = before->next;	
-  	}
-  	list_elem_t * removing = before->next;
-  	before->next=removing->next;
-  	free(removing);
-  	
+  else
+  {
+    for (int i = 0; i < indexremove - 1; i++)
+    {
+      before = before->next;
+    }
+    list_elem_t *removing = before->next;
+    before->next = removing->next;
+    free(removing);
   }
   return 0;
-  
 }
-
 
 /*
  * SYNOPSYS :
@@ -391,37 +424,37 @@ remove_element_2(list_elem_t * * ppl, int value) {
  * PARAMETRES :
  *   list_elem_t ** l : pointer to the pointer of the head of the list
  * RESULTAT :
- *   aucun 
+ *   aucun
  */
-void
-reverse_list(list_elem_t * * l) {
+void reverse_list(list_elem_t **l)
+{
 
-	list_elem_t * current = *l;
-	// la liste est vide et n'es pas composé d'un seul élément
-	if ((current != NULL) && (current->next != NULL)){
-	
-		// on considère que l'élement avant la tete sera NULL donc on l'initialise comme tel
-		list_elem_t * prev_elem = NULL;
-		
-		// on reserve la mémoire pour les éléments suivant le courant
-		list_elem_t * next_elem;
-	
-		while (current != NULL){
-		
-			// on affecte l'élément suivant
-			next_elem = current -> next ;
-			// on change le pointeur next du courant
-			current->next = prev_elem;
-			// on actualise l'élement précédent et courant
-			prev_elem = current;
-			current = next_elem;	
-		}
-		
-		*l = prev_elem;
-	}
+  list_elem_t *current = *l;
+  // la liste est vide et n'es pas composé d'un seul élément
+  if ((current != NULL) && (current->next != NULL))
+  {
+
+    // on considère que l'élement avant la tete sera NULL donc on l'initialise comme tel
+    list_elem_t *prev_elem = NULL;
+
+    // on reserve la mémoire pour les éléments suivant le courant
+    list_elem_t *next_elem;
+
+    while (current != NULL)
+    {
+
+      // on affecte l'élément suivant
+      next_elem = current->next;
+      // on change le pointeur next du courant
+      current->next = prev_elem;
+      // on actualise l'élement précédent et courant
+      prev_elem = current;
+      current = next_elem;
+    }
+
+    *l = prev_elem;
+  }
 }
-
-
 
 /*
  * SYNOPSYS :
@@ -433,54 +466,49 @@ reverse_list(list_elem_t * * l) {
  * PARAMETRES :
  *   list_elem_t ** l : pointer to the pointer of the head of the list
  * RESULTAT :
- *   aucun 
+ *   aucun
  */
- 
-void
-reverse_list_2(list_elem_t * * l) {
-	
-	list_elem_t * new_head = NULL;
-	list_elem_t * current = *l;
-	while(current != NULL){
-		insert_head(&new_head, current->value) ;
-		current = current -> next ;
-	}
-	erase_list(l);
-	*l = new_head;
+
+void reverse_list_2(list_elem_t **l)
+{
+
+  list_elem_t *new_head = NULL;
+  list_elem_t *current = *l;
+  while (current != NULL)
+  {
+    insert_head(&new_head, current->value);
+    current = current->next;
+  }
+  erase_list(l);
+  *l = new_head;
 }
-
-
-
-
-
 
 /*
  * SYNOPSYS :
  *   void erase_list(list_elem_t * * ppl)
  * DESCRIPTION :
- *   Before exiting the program, it erases the elements of the list 
+ *   Before exiting the program, it erases the elements of the list
  * PARAMETRES :
  *   list_elem_t ** l : pointer to the pointer of the head of the list
  * RESULTAT :
- *   aucun 
+ *   aucun
  */
-void
-erase_list(list_elem_t * * ppl) {
+void erase_list(list_elem_t **ppl)
+{
 
-	list_elem_t * current = *ppl;
+  list_elem_t *current = *ppl;
 
-	if( current != NULL) {
-		list_elem_t * next_element =  current -> next;
-		while(current->next != NULL){
+  if (current != NULL)
+  {
+    list_elem_t *next_element = current->next;
+    while (current->next != NULL)
+    {
 
-			free(current);
-			current = next_element;
-			next_element = current->next;
-
-      }
       free(current);
+      current = next_element;
+      next_element = current->next;
     }
-	*ppl = NULL;
-	
-
+    free(current);
+  }
+  *ppl = NULL;
 }
--- src/test_list.c
+++ src/test_list.c
@@ -16,11 +16,11 @@
 #include "list.h"
 
 /* Compute the number of elements of the list */
-int
-list_size(list_elem_t * p_list)
+int list_size(list_elem_t *p_list)
 {
   int nb = 0;
-  while (p_list != NULL) {
+  while (p_list != NULL)
+  {
     nb += 1;
     p_list = p_list->next;
   }
@@ -28,122 +28,125 @@ list_size(list_elem_t * p_list)
 }
 
 /* Print the elements of the list */
-void
-print_list(list_elem_t* p_list) {
-  list_elem_t * pl = p_list;
+void print_list(list_elem_t *p_list)
+{
+  list_elem_t *pl = p_list;
   printf("The list contains %d element(s)\n", list_size(p_list));
-  while(pl != NULL) {
-    printf("[%d]",pl->value);
+  while (pl != NULL)
+  {
+    printf("[%d]", pl->value);
     pl = pl->next;
-    if (pl != NULL) {
+    if (pl != NULL)
+    {
       printf("->");
     }
   }
 }
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-  list_elem_t * head = NULL;	// The pointer to the head of the list
+  list_elem_t *head = NULL; // The pointer to the head of the list
   char menu[] =
-    "\n Program of chained list \n"\
-    "  'h/t' : Insert an element to the head/tail of the list\n"\
-    "  'f'   : search of a list element\n"\
-    "  's'   : suppression of a list element\n"\
-    "  'r'   : reverse the order of the list elements\n"\
-    "  'x'   : exit the program\n"\
-    " What is your choice ?";
-  int choice=0;				// choice from the menu
-  int value=0;		    // inserted value
-  int index=0;			
-  printf("%s",menu);
+      "\n Program of chained list \n"
+      "  'h/t' : Insert an element to the head/tail of the list\n"
+      "  'f'   : search of a list element\n"
+      "  's'   : suppression of a list element\n"
+      "  'r'   : reverse the order of the list elements\n"
+      "  'x'   : exit the program\n"
+      " What is your choice ?";
+  int choice = 0; // choice from the menu
+  int value = 0;  // inserted value
+  int index = 0;
+  printf("%s", menu);
   fflush(stdout);
 
-  while(1) {
+  while (1)
+  {
     fflush(stdin);
     choice = getchar();
     printf("\n");
 
-    switch(choice) {
-    case 'H' :
-    case 'h' :
+    switch (choice)
+    {
+    case 'H':
+    case 'h':
       printf("Value of the new element ? ");
-      scanf("%d",&value);
-      if (insert_head(&head,value)!=0) {
-	printf("Error : impossible to add the element %d\n",value);
+      scanf("%d", &value);
+      if (insert_head(&head, value) != 0)
+      {
+        printf("Error : impossible to add the element %d\n", value);
       };
       break;
 
-    case 'T' :
-    case 't' :
+    case 'T':
+    case 't':
       printf("Value of the new element ? ");
-      scanf("%d",&value);
-      if (insert_tail(&head,value)!=0) {
-	      printf("Error : impossible to add the element %d\n",value);
+      scanf("%d", &value);
+      if (insert_tail(&head, value) != 0)
+      {
+        printf("Error : impossible to add the element %d\n", value);
       };
       break;
 
-
-    case 'F' :
-    case 'f' :
+    case 'F':
+    case 'f':
       printf("index of the searched element ? ");
-      scanf("%d",&index);
-      list_elem_t * elem = find_element(head,index);
-      if ( elem == NULL) {
-	      printf("Error : impossible to find the element %d\n",index);
+      scanf("%d", &index);
+      list_elem_t *elem = find_element(head, index);
+      if (elem == NULL)
+      {
+        printf("Error : impossible to find the element %d\n", index);
       }
-      else {
+      else
+      {
         printf(" searched element is %d\n", elem->value);
       }
       break;
 
-    // case 'V' :
-    // case 'v' :
-    //   printf("index of the searched element ? ");
-    //   scanf("%d",&value);
-    //   list_elem_t * elembis = find_first_element_with_value(head,value);
-    //   if ( elembis == NULL) {
-	  //     printf("Error : impossible to find a element containing %d\n",value);
-    //   }
-    //   else {
-    //     printf(" searched element is %d\n", value);
-    //   }
-    //   break;
+      // case 'V' :
+      // case 'v' :
+      //   printf("index of the searched element ? ");
+      //   scanf("%d",&value);
+      //   list_elem_t * elembis = find_first_element_with_value(head,value);
+      //   if ( elembis == NULL) {
+      //     printf("Error : impossible to find a element containing %d\n",value);
+      //   }
+      //   else {
+      //     printf(" searched element is %d\n", value);
+      //   }
+      //   break;
 
-
-
-    case 'V' :
-    case 'v' :
+    case 'V':
+    case 'v':
       printf("index of the searched element ? ");
-      scanf("%d",&value);
-      int index = find_index_element(head,value);
-      if ( index == -1) {
-	      printf("Error : impossible to find a element containing %d\n",value);
+      scanf("%d", &value);
+      int index = find_index_element(head, value);
+      if (index == -1)
+      {
+        printf("Error : impossible to find a element containing %d\n", value);
       }
-      else {
+      else
+      {
         printf(" index of  %d is %d\n", value, index);
       }
       break;
 
-
-
-    case 's' :
-    case 'S' :
-            printf("Value of the element to remove ? ");
-      scanf("%d",&value);
-      if (remove_element(&head,value)!=0) {
-	      printf("Error : impossible to remove the element %d\n",value);
+    case 's':
+    case 'S':
+      printf("Value of the element to remove ? ");
+      scanf("%d", &value);
+      if (remove_element(&head, value) != 0)
+      {
+        printf("Error : impossible to remove the element %d\n", value);
       };
 
-
-    case 'r' :
-    case 'R' :
-	reverse_list(&head);
+    case 'r':
+    case 'R':
+      reverse_list(&head);
       break;
 
-
-    case 'x' :
-    case 'X' :
+    case 'x':
+    case 'X':
       erase_list(&head);
       print_list(head);
       printf("list erased");
@@ -155,10 +158,7 @@ main(int argc, char **argv)
     print_list(head);
 
     getchar(); // to consume a return character and avoid double display of the menu
-    printf("%s\n",menu);
+    printf("%s\n", menu);
   }
   return 0;
 }
-
-
-
